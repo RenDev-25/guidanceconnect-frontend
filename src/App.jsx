700d84873c0f52872cf4f184d87b4e86adc03cd2
Rennel24 <24-00424@g.batstate-u.edu.ph>
2026-09-25 17:36:52 +0800

    Initial commit: GuidanceConnect frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Routes will be imported here in later stages */}
+      <h1>GuidanceConnect Architecture Ready</h1>
+    </div>
+  );
+}
+
+export default App;
